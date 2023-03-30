4c382adcbd079bbf95e5a29bc72574ea5a270f63
Shawn Hsu <ambersun1019.shawn@gmail.com>
2023-03-30 17:48:26 +0800

    Add basicNFT deploy scripts

---
+++ deploy/deploy.ts
@@ -0,0 +1,29 @@
+import { verify } from "./../utils/verify";
+import { ChainMapping, DevelopmentChains } from "./../helper-hardhat.config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/dist/types";
+
+const deployBasicNFT: DeployFunction = async (
+    hre: HardhatRuntimeEnvironment
+) => {
+    const { getNamedAccounts, deployments, network } = hre;
+    const { deploy } = deployments;
+    const { deployer } = await getNamedAccounts();
+    const chainID = network.config.chainId!;
+
+    const nft = await deploy("BasicNFT", {
+        from: deployer,
+        args: [],
+        log: true,
+        waitConfirmations: 1
+    });
+
+    if (!DevelopmentChains.includes(ChainMapping[chainID])) {
+        console.log("Test network found, verify on etherscan");
+        await verify(nft.address, []);
+    }
+};
+
+deployBasicNFT.tags = ["basicNFT", "all"];
+
+export default deployBasicNFT;
