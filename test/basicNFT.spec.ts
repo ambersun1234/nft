fc2f19e5596449bd1ba8e6fa560263c988339c0e
Shawn Hsu <ambersun1019.shawn@gmail.com>
2023-04-01 22:32:27 +0800

    Add development chain branch to basic nft test

--- test/basicNFT.spec.ts
+++ test/basicNFT.spec.ts
@@ -1,72 +1,76 @@
-import { ethers, deployments } from "hardhat";
+import { ethers, deployments, network } from "hardhat";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { assert, expect } from "chai";
 
 import { BasicNFT } from "./../typechain-types/contracts/BasicNFT";
 
-describe("BasicNFT", () => {
-    const tokenURI =
-        "ipfs.io/ipfs/bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
-    let nft: BasicNFT;
-    let deployer: SignerWithAddress;
+import { ChainMapping, DevelopmentChains } from "./../helper-hardhat.config";
 
-    beforeEach(async () => {
-        await deployments.fixture(["basicNFT"]);
-        deployer = (await ethers.getSigners())[0];
-        nft = await ethers.getContract("BasicNFT", deployer);
-    });
+!DevelopmentChains.includes(ChainMapping[network.config.chainId!])
+    ? describe.skip
+    : describe("BasicNFT", () => {
+          const tokenURI =
+              "ipfs.io/ipfs/bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+          let nft: BasicNFT;
+          let deployer: SignerWithAddress;
 
-    describe("constructor", () => {
-        it("Should successfully initialize", async () => {
-            assert.equal((await nft.getTokenCounter()).toString(), "0");
-        });
-    });
+          beforeEach(async () => {
+              await deployments.fixture(["basicNFT"]);
+              deployer = (await ethers.getSigners())[0];
+              nft = await ethers.getContract("BasicNFT", deployer);
+          });
 
-    describe("tokenURI", () => {
-        it("Should return constant uri", async () => {
-            assert.equal(await nft.tokenURI(1), tokenURI);
-            assert.equal(await nft.tokenURI(2), tokenURI);
-        });
-    });
+          describe("constructor", () => {
+              it("Should successfully initialize", async () => {
+                  assert.equal((await nft.getTokenCounter()).toString(), "0");
+              });
+          });
 
-    describe("mint", () => {
-        it("Should mint the first token", async () => {
-            const txResponse = await nft.mint();
-            const txReceipt = await txResponse.wait(1);
-            assert.equal(
-                parseInt(txReceipt.logs[0].topics[3]).toString(),
-                "0"
-            );
-            assert.equal((await nft.getTokenCounter()).toString(), "1");
-        });
+          describe("tokenURI", () => {
+              it("Should return constant uri", async () => {
+                  assert.equal(await nft.tokenURI(1), tokenURI);
+                  assert.equal(await nft.tokenURI(2), tokenURI);
+              });
+          });
 
-        it("Should mint n token", async () => {
-            for (let i = 0; i < 10; i++) {
-                const response = await nft.mint();
-                const receipt = await response.wait(1);
-                assert.equal(
-                    parseInt(receipt.logs[0].topics[3]).toString(),
-                    i.toString()
-                );
-                assert.equal(
-                    (await nft.getTokenCounter()).toString(),
-                    (i + 1).toString()
-                );
-            }
-            assert.equal((await nft.getTokenCounter()).toString(), "10");
-        });
+          describe("mint", () => {
+              it("Should mint the first token", async () => {
+                  const txResponse = await nft.mint();
+                  const txReceipt = await txResponse.wait(1);
+                  assert.equal(
+                      parseInt(txReceipt.logs[0].topics[3]).toString(),
+                      "0"
+                  );
+                  assert.equal((await nft.getTokenCounter()).toString(), "1");
+              });
 
-        it("Others should be able to mint", async () => {
-            const people = (await ethers.getSigners())[1];
+              it("Should mint n token", async () => {
+                  for (let i = 0; i < 10; i++) {
+                      const response = await nft.mint();
+                      const receipt = await response.wait(1);
+                      assert.equal(
+                          parseInt(receipt.logs[0].topics[3]).toString(),
+                          i.toString()
+                      );
+                      assert.equal(
+                          (await nft.getTokenCounter()).toString(),
+                          (i + 1).toString()
+                      );
+                  }
+                  assert.equal((await nft.getTokenCounter()).toString(), "10");
+              });
 
-            const peopleContract = nft.connect(people);
-            const receipt = await peopleContract.mint();
-            await receipt.wait(1);
-            assert.equal(receipt.value.toString(), "0");
-        });
+              it("Others should be able to mint", async () => {
+                  const people = (await ethers.getSigners())[1];
 
-        it("Should emit transfer event", async () => {
-            await expect(nft.mint()).to.emit(nft, "Transfer")
-        })
-    });
-});
+                  const peopleContract = nft.connect(people);
+                  const receipt = await peopleContract.mint();
+                  await receipt.wait(1);
+                  assert.equal(receipt.value.toString(), "0");
+              });
+
+              it("Should emit transfer event", async () => {
+                  await expect(nft.mint()).to.emit(nft, "Transfer");
+              });
+          });
+      });
